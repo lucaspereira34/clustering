2bcbef7c1fca13deec9c957c666fe3c6816aa71d
Lucas Pereira <lspereira340@gmail.com>
2024-08-13 22:47:29 -0300

    Edit country.py

--- country.py
+++ country.py
@@ -59,4 +59,62 @@ fig.show()
 
 #%% Standardize variables
 
-pat = data.apply(zscore, ddof=1)
+# Apply ZScore to all variables, setting average=0 and std=1 to each variable
+z_data = data.apply(zscore, ddof=1)
+
+#%% Agglomerative hierarchical clustering: euclidean distance + single linkage
+
+# Distances
+euclidian_dist = pdist(z_data, metric='euclidean')
+
+# Metrics (distances)
+    ## euclidean
+    ## sqeuclidean
+    ## chebyshev
+    ## canberra
+    ## correlation
+    
+# Linkage methods
+    ## single
+    ## complete
+    ## average
+    
+# Create dendrogram
+plt.figure(figsize=(16,8))
+single_link = sch.linkage(z_data, method='single', metric='euclidean')
+dendrogram_s = sch.dendrogram(single_link)
+plt.title('Single Linkage Dendrogram', fontsize=16)
+plt.xlabel('Countries', fontsize=16)
+plt.ylabel('Euclidian Distance', fontsize=16)
+plt.show()
+
+#%% Agglomerative hierarchical clustering: euclidean distance + average linkage
+
+plt.figure(figsize=(16,8))
+avg_link = sch.linkage(z_data, method='average', metric='euclidean')
+dendrogram_a = sch.dendrogram(avg_link)
+plt.title('Average Linkage Dendrogram', fontsize=16)
+plt.xlabel('Countries', fontsize=16)
+plt.ylabel('Euclidean Distance', fontsize=16)
+plt.show()
+
+#%% Agglomerative hierachical clustering: euclidean distance + complete linkage
+
+plt.figure(figsize=(16,8))
+complete_link = sch.linkage(z_data, method='complete', metric='euclidean')
+dendrogram_c = sch.dendrogram(complete_link)
+plt.title('Complete Linkage Dendrogram', fontsize=16)
+plt.xlabel('Countries', fontsize=16)
+plt.ylabel('Euclidean Distance', fontsize=16)
+plt.show()
+
+# Create a new variable that stores a cluster label for each index
+cluster = AgglomerativeClustering(n_clusters = 5, metric = 'euclidean', linkage = 'complete')
+set_cluster = cluster.fit_predict(z_data)
+
+data['cluster'] = set_cluster
+data.cluster = data.cluster.astype('category')
+
+z_data['cluster'] = set_cluster
+z_data.cluster = z_data.cluster.astype('category')
+
